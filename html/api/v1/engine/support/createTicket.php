480284b889b0733a45aff52e64f6a38a88317834
Alexander Alibaba Atlas <159730217+AlibabaTheGreat@users.noreply.github.com>
2025-12-06 20:03:47 -0300

    removed support endpoints

--- html/api/v1/engine/support/createTicket.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once(__DIR__ . "/../../engine/engine.php");
-
-use Kickback\Backend\Controllers\TicketController;
-
-OnlyPOST();
-
-return TicketController::createTicketFromRequest($_POST, $_FILES);
-?>
--- html/api/v1/support/createTicket.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$resp = require(__DIR__ . '/../engine/support/createTicket.php');
-$resp->Return();
-?>
