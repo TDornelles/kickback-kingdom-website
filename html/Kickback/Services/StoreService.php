23511c5f592eaae02d7ef8951c80a2a9062fd650
Hans Atlas <160430514+sgtLongs@users.noreply.github.com>
2025-12-06 19:12:35 -0500

    class distinguition for vItemFromJSON in StoreService

--- html/Kickback/Services/StoreService.php
+++ html/Kickback/Services/StoreService.php
@@ -21,6 +21,7 @@ use Kickback\Backend\Views\vTransaction;
 use Kickback\Common\Primitives\Obj;
 use Kickback\Services\ApiV2\Endpoint;
 use LogicException;
+use stdClass;
 
 class StoreService
 {
@@ -944,9 +945,19 @@ class StoreService
         $vItem->name = $item->name;
         $vItem->description = $item->description;
 
-        $vItem->iconSmall = static::vMediaFromJson_FullURL($item->iconSmall->url);
-        $vItem->iconBig = static::vMediaFromJson_FullURL($item->iconBig->url);
-        $vItem->iconBack = static::vMediaFromJson_FullURL($item->iconBack->url);
+        if($item->iconSmall instanceof stdClass)
+        {
+            $vItem->iconSmall = static::vMediaFromJson_FullURL($item->iconSmall->url);
+            $vItem->iconBig = static::vMediaFromJson_FullURL($item->iconBig->url);
+            $vItem->iconBack = static::vMediaFromJson_FullURL($item->iconBack->url);
+        }
+        else
+        {
+            $vItem->iconSmall = static::vMediaFromJson_FullURL($item->iconSmall["url"]);
+            $vItem->iconBig = static::vMediaFromJson_FullURL($item->iconBig["url"]);
+            $vItem->iconBack = static::vMediaFromJson_FullURL($item->iconBack["url"]);
+        }
+        
         $vItem->fungible = $item->fungible;
 
         return $vItem;
