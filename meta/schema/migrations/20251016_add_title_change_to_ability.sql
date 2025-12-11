6a2a2a3a17b74724d53a091bf7533a7c88e51b67
Alexander Alibaba Atlas <159730217+AlibabaTheGreat@users.noreply.github.com>
2025-12-11 14:23:29 -0300

    Update 20251016_add_title_change_to_ability.sql

--- meta/schema/migrations/20251016_add_title_change_to_ability.sql
+++ meta/schema/migrations/20251016_add_title_change_to_ability.sql
@@ -1,8 +1,12 @@
 ALTER TABLE ability
     ADD COLUMN `title_change` varchar(25) NOT NULL DEFAULT '' AFTER `level_multiplier`;
 
+SET FOREIGN_KEY_CHECKS = 0;
+
 ALTER TABLE item_ability
     DROP FOREIGN KEY `item_ability_ability_fk`,
     ADD CONSTRAINT `item_ability_ability_fk`
         FOREIGN KEY (`ability_id`) REFERENCES `ability` (`Id`)
         ON DELETE CASCADE ON UPDATE CASCADE;
+
+SET FOREIGN_KEY_CHECKS = 1;
