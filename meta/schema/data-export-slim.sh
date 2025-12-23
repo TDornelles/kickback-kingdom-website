d34b2e8dbf7751af38d568a8742a7e999443a329
Alexander Alibaba Atlas <159730217+AlibabaTheGreat@users.noreply.github.com>
2025-12-23 20:45:56 -0300

    added extension

--- meta/schema/data-export-slim.sh
+++ meta/schema/data-export-slim.sh
@@ -12,7 +12,7 @@ KKDB_EXPORT_PATH="${KKDB_EXPORT_PATH:-$HOME/kickback-kingdom-slim-database-backu
 KKDB_USER_NAME="${KKDB_USER_NAME:-root}"
 KKDB_DATABASE_NAME="${KKDB_DATABASE_NAME:-kickbackdb}"
 
-STRUCTURE_ONLY_TABLES="analytics share_purchases merchant_share_statement ticket ticket_assignment ticket_attachment ticket_category ticket_comment ticket_tag transaction"
+STRUCTURE_ONLY_TABLES="analytics share_purchase merchant_share_statement ticket ticket_assignment ticket_attachment ticket_category ticket_comment ticket_tag transaction"
 
 if [ -f "$KKDB_EXPORT_PATH" ]; then
     # Try to get last-modified timestamp off of existing file.
